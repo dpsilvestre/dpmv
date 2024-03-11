f3584ab66b953dd0d3d26e0e110ca1f1ace55ca3
dpsilvestre <dp2132332@gmail.com>
2024-03-11 11:47:58 -0300

    Tutorial pt2

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f3bf6504'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3eqrbtnhlsg"
+    "revision": "0.nb34j8ciojo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
